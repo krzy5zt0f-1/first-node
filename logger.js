574f9cc019dbe1a5236db82c8db781b8b6b4d63a
Krzysztof Kasprzak <timmmc88@gmail.com>
2020-11-25 07:50:27 +0000

    extending emmiter exercise completed

--- logger.js
+++ logger.js
@@ -1,17 +1,19 @@
-console.log(__filename);
-console.log(__dirname);
+
 const EventEmitter = require('events');
 //use of events module which is a class
-const emitter = new EventEmitter();
+
 var url = 'http://fakrlogger.io/log';
 
-function log(message) {
-  //as if sending HTTP request;
-  console.log(message);
-  // Raised an event
-  emitter.emit('messageLogged', {id: 1, url: 'http://'});
+class Logger extends EventEmitter {
+  log(message) {
+    //as if sending HTTP request;
+    console.log(message);
+    // Raised an event
+    this.emit('messageLogged', {id: 1, url: 'http://'});
+  }
 }
+
 
 // adding the method log() to exports to be used in other modules
 
-module.exports = log;
+module.exports = Logger;
